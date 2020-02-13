08ea8b8342a40ff226250a3d1e07faf8816080c6
ammorcos <60280446+ammorcos@users.noreply.github.com>
2020-02-12 22:11:50 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -39,7 +39,7 @@ def lists():
     p = n.split(" ")
 
     # Get all the items past the first of the third substring
-    r = p[3:]
+    r = p[2][1:]
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
